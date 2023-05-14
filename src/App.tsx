988287f555c6c93122d9cbb03f7c3a9820c1c20e
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-05-13 20:45:05 -0400

    testing CSS and clock displays

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,27 @@ import './App.css';
 import { Clock } from './Models/Clocks';
 import './Clocks.css'
 import { Header } from './Header';
+import ClockForm from './Components/ClockForm/ClockForm';
+import { ClockDisplay } from './ClockDisplay';
+import AnalogClock from './AnalogClock';
+import DigitalClock from './DigitalClock';
 
 
 function App() {
 
-  const [clocks, setClocks] = useState<Clock[]>([
-
-
-  ]);
+  const [clocks, setClocks] = useState<Clock[]>([]);
 
   return (
     <div className="App">
-      
     <Header></Header>
-    
+      <div className='headings'>
+        <h2>Settings</h2>
+        <h2>Clocks</h2>
+     </div>
+     <button className='addClock'>Add Clock</button>
+     {/* <ClockDisplay></ClockDisplay> */}
+     <div className='analog'><AnalogClock></AnalogClock></div>
+     <div className='digital'><DigitalClock></DigitalClock></div>
     </div>
   );
 }
